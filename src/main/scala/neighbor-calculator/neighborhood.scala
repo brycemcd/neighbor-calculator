c32b5638203070b9ce296987f5eb223b18bfb56d
Bryce McDonnell <bryce@bridgetownint.com>
2016-06-12 09:35:32 -0400

    WIP - calculating nearest neighbors

--- src/main/scala/neighbor-calculator/neighborhood.scala
+++ src/main/scala/neighbor-calculator/neighborhood.scala
@@ -3,12 +3,26 @@ package NeighborCalculator
 import scala.annotation.tailrec
 import breeze.linalg._
 
+
 object Neighbor {
-  private lazy val mat = DenseMatrix(
-    DenseVector(1,0,0,1,1),
-    DenseVector(1,0,0,1,1),
-    DenseVector(0,1,1,0,0),
-    DenseVector(1,1,1,0,0)
-  )
-  //def calculate = calcNeighbors(mat)
+  // TODO: put this in a package object?
+  type NeighborSimilarity = Tuple2[(Int, Int), Double]
+
+  // TODO: move this to Similarity Calculator
+  def compare(one: Tuple2[Int, List[Any]],
+              two: Tuple2[Int, List[Any]]) = {
+  val (left_id, left_features) = if(one._1 < two._1) one else two
+  val (right_id, right_features) = if(one._1 < two._1) two else one 
+
+  val sim = SimilarityCalculator.binarySimilarity(left_features,
+                                        right_features,
+                                        SimilarityCalculator.jacaard)
+  ((left_id, right_id), sim)
+  }
+
+  def nearest(calculateNeighbors: Seq[NeighborSimilarity]) =  {
+    val root = calculateNeighbors.head._1._1
+    val sorted = calculateNeighbors sortBy { _._2 } map (_._1._2)
+    (root, sorted.reverse)
+  }
 }
--- src/test/scala/neighbor-calculator/neightborhood-test.scala
+++ src/test/scala/neighbor-calculator/neightborhood-test.scala
@@ -1,7 +1,45 @@
+package NeighborCalculator.Test
+
 import org.scalatest.FunSpec
 
 import breeze.linalg.DenseVector
+import NeighborCalculator.Neighbor
 import NeighborCalculator.SimilarityCalculator
 
-class NeightborSpec extends FunSpec {
+class NeighborSpec extends FunSpec {
+  describe("Neighbor Object") {
+    lazy val list1 = List(1,0,Nil,Nil,0)
+    lazy val list2 = List(1,1,1,Nil,Nil)
+
+    lazy val personOne = (1, list1)
+    lazy val personTwo = (2, list2)
+
+    describe("calculateNeighbor") {
+      val sim = SimilarityCalculator.binarySimilarity(list1,
+                                                      list2,
+                                            SimilarityCalculator.jacaard)
+      it("returns a list of similar users") {
+        val compared = Neighbor.compare(personOne, personTwo)
+        assert(compared == ((1,2), sim))
+      }
+
+      it("returns a list of sorted user ids with similarity") {
+
+        val compared = Neighbor.compare(personTwo, personOne)
+        assert(compared == ((1,2), sim))
+      }
+    }
+    describe("nearest") {
+      lazy val simOne = ((1,2), 0.7)
+      lazy val simTwo = ((1,3), 0.4)
+      lazy val simThree = ((1,4), 0.8)
+
+      lazy val simList = List(simOne, simTwo, simThree)
+
+      it("takes a list of calculated similarities and returns the sorted n nearest") {
+        val sims = Neighbor.nearest(simList)
+        assert(sims == (1, List(4,2,3)))
+      }
+    }
+  }
 }
